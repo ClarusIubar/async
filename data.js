3bd37dfc22016e90ebfe18fda610ee0aa2a12abe
nobamb <jsh12345644@gmail.com>
2026-02-23 17:03:12 +0900

    배열 객체형태로 값들 저장

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "슈퍼맨", age: 30, nickname: "슈퍼" },
+  { name: "배트맨", age: 32, nickname: "박쥐" },
+  { name: "맨투맨", age: 25, nickname: "옷" },
+  { name: "원펀맨", age: 27, nickname: "대머리" },
+];
