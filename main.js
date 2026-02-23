417686d85193123ace1a4cdff5dbbbe8f699f6cf
nobamb <jsh12345644@gmail.com>
2026-02-23 18:53:24 +0900

    main.js에서 비동기로 server.py에서 데이터를 받아서 콘솔에 출력하게 함

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import data from "./data.js";
 const submitButton = document.querySelector("form button");
 
 // 파일 연결 테스트
-const ajaxTest = (e) => {
+// async를 통해 비동기 지정
+// await 전까지는 비동기로 행동
+const ajaxTest = async (e) => {
   e.preventDefault();
 
   // select 요소 가져오기
@@ -37,8 +39,31 @@ const ajaxTest = (e) => {
       break;
   }
 
+
   //   dataValue 출력
-  console.log(dataValue);
+  //   console.log(dataValue);
+
+  // 데이터(dataValue)를 server로 전달
+  // 비동기 방식(서버 통신 부터는 시간이 걸리기에
+  //   await를 통해 기다리도록 하기
+  const response = await fetch("http://localhost:8000/", {
+    // 전송 타입
+    // 전송할 것이기에 post
+    method: "POST",
+    // json 형태로 전달하려고 할 것이기에 application/json
+    headers: { "content-type": "application/json" },
+    // 전달할 데이터는 body
+    // json으로 변환(json.stringify)
+    body: JSON.stringify(dataValue),
+  });
+
+  // data를 받으면 다시 객체화
+  const result = await response.json();
+
+  // result가 지정이 되면 실행
+
+  console.log(result)
+
 
   console.log("통신 성공");
 };
