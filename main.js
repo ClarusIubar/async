8f9a8747cbd5ded89705f4d78dd616b3e2a66a0d
nobamb <jsh12345644@gmail.com>
2026-02-23 16:37:28 +0900

    select의 값 가져옴

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const ajaxTest = (e) => {
 
     e.preventDefault()
+    
+    console.log(e)
+
+    // select 요소 가져오기
+    const selectElement = document.querySelector("select")
+    // select의 값 출력
+    console.log(selectElement.value)
+
+
     console.log("통신 성공")
 
 }
